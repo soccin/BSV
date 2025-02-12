4092088b3e626862a99c56b9403387656ff2655c
Nicholas D. Socci <soccin@mskcc.org>
2025-02-12 04:00:41 -0500

    ver 1 run1Samp

--- runDelly1Samp.sh
+++ runDelly1Samp.sh
@@ -20,17 +20,43 @@ BASE=$(basename $TUMOR | sed 's/.bam//' | sed 's/___MD.*//')
 echo $TUMOR
 echo $BASE
 
-GENOME=/juno/depot/assemblies/H.sapiens/b37/b37.fasta
-EXCL=$SDIR/delly/excludeTemplates/human.b37.excl.tsv
+GENOME=/juno/bic/depot/assemblies/H.sapiens/b37/b37.fasta
+EXCL=$SDIR/rsrc/delly/human.b37.excl.tsv
 
-DELLY=delly_v1.2.6_linux_x86_64bit
+DELLY=$(ls $SDIR/bin/delly_*linux_x86_* | sort -V | tail -1)
 
-$SDIR/bin/$DELLY \
+ODIR=out/delly/$BASE
+
+mkdir -p $ODIR
+
+$DELLY \
     call \
     -x $EXCL \
     -g $GENOME \
-    -o ${BASE}.bcf \
+    -o ${ODIR}/${BASE}.bcf \
     $TUMOR
 
-bcftools view ${BASE}.bcf >${BASE}___FILTER.vcf
+bcftools view ${ODIR}/${BASE}.bcf >${ODIR}/${BASE}.vcf
 
+cat <<-EOF > $ODIR/${BASE}.run.log
+SDIR: $SDIR
+GENOME: $GENOME
+EXCL: $EXCL
+TUMOR: $TUMOR
+ODIR: $ODIR
+DELLY: $DELLY
+
+BCFTOOLS: $(bcftools --version | head -1)
+
+Script: $0 $*
+
+$DELLY \
+    call \
+    -x $EXCL \
+    -g $GENOME \
+    -o ${ODIR}/${BASE}.bcf \
+    $TUMOR
+
+bcftools view ${ODIR}/${BASE}.bcf >${ODIR}/${BASE}.vcf
+
+EOF
